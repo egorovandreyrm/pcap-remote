63f6ba03f38abdbdf4437a77fe30bc17a461868c
Andrey Egorov <egorovandreyrm@gmail.com>
2020-01-12 19:28:34 +0300

    added license

--- app/src/main/java/com/pcapremote/DashboardFragment.kt
+++ app/src/main/java/com/pcapremote/DashboardFragment.kt
@@ -1,3 +1,22 @@
+/*
+    This file is part of PCAP Remote.
+
+    PCAP Remote is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PCAP Remote is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with PCAP Remote. If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2019 by Andrey Egorov
+*/
+
 package com.pcapremote
 
 import android.app.AlertDialog
@@ -21,7 +40,6 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
-
 
 class DashboardFragment : Fragment(), SharedPreferences.OnSharedPreferenceChangeListener {
 
--- app/src/main/java/com/pcapremote/WebViewActivity.kt
+++ app/src/main/java/com/pcapremote/WebViewActivity.kt
@@ -1,3 +1,22 @@
+/*
+    This file is part of PCAP Remote.
+
+    PCAP Remote is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PCAP Remote is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with PCAP Remote. If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2019 by Andrey Egorov
+*/
+
 package com.pcapremote
 
 import android.app.AlertDialog
@@ -15,8 +34,6 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_webview.*
 import timber.log.Timber
 
-
-// tries to load the url, if fails, renders a message suggesting to open the url in a browser.
 class WebViewActivity : AppCompatActivity() {
 
     companion object {
